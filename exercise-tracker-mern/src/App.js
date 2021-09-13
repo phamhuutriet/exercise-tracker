90d30cf31d210742b36bd2719ea3b0853e34712e
Triet Pham <triet.ph1999@gmail.com>
2021-09-12 22:03:30 -0600

    Hook Front End and Backend

--- exercise-tracker-mern/src/App.js
+++ exercise-tracker-mern/src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
-import React from 'react'
-import "boostrap/dist/css/boostrap.min.css"
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercises.component";
+import CreateUser from "./components/create-user.component";
+import Navbar from "./components/navbar.component";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
---
+++ exercise-tracker-mern/src/components/exercises-list.component.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Exercise = props => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>
+      <Link to={`/edit/${props.exercise._id}`}>edit</Link> | <a href="#" onClick={() => props.deleteExercise(props)}>delete</a>
+    </td>
+  </tr>
+)
+
+
+export class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = {
+      exercise: [],
+    };
+  }
+
+  // Render once at the beginning and update all exercises into a list
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/exercises")
+      .then((res) => {
+        this.setState({ exercise: res.data });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  deleteExercise(id) {
+    axios
+      .delete("http://localhost:5000/exercises")
+      .then((res) => console.log(res.data));
+
+    this.setState({
+      exercise: this.state.exercise.filter((ele) => ele._id !== id), // Because in DB, the id of each ele is "_id"
+    });
+  }
+
+  exercisesList() {
+    return this.state.exercise.map(curExercise => {
+      return <Exercise exercise={curExercise} deleteExercise={this.deleteExercise} key={curExercise._id} />
+    })
+  }
+
+  render() {
+    return (
+    <div>
+      <h3>Logged Exercises</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>Username</th>
+            <th>Description</th>
+            <th>Duration</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.exercisesList()}
+        </tbody>
+      </table>
+    </div>
+  )}
+}
+
+export default ExercisesList;
